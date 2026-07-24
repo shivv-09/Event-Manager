39271cbf7d01aea35f7b6815a916dd2a85fcbc03
Shivpratap Ishwar Patil <shivpratappatil414@gmail.com>
2026-07-24 13:08:47 +0530

    Fix base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Event_Manager/',
+  base: '/Event-Manager/',
   plugins: [react(), tailwindcss()],
   base: '/Event-Manager/',
   server: {
